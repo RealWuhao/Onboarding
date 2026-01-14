8f7e52f4be0f1a54dd030ee160a1993824bae4bc
Wuhao Chen <wuhao.chen95@gmail.com>
2026-01-14 09:09:01 +0800

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -1,3 +1,17 @@
 ﻿##Test Script file
 hostname
 
+#check version
+git --version
+
+#Turn any folder into a repo
+git status
+
+# config
+git config --global --list
+git config --global user.name "Wuhao Chen"
+git config --global user.email "wuhao.chen95@gmail.com"
+
+#many other settings
+git config --global init.defaultBranch main
+
