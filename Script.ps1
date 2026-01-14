86d0c032e8ba45ebf403f56936e815f10ef79509
Wuhao Chen <wuhao.chen95@gmail.com>
2026-01-14 10:13:15 +0800

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -15,3 +15,11 @@ git config --global user.email "wuhao.chen95@gmail.com"
 #many other settings
 git config --global init.defaultBranch main
 
+#standard git cmds
+git status 
+git add -A 
+git commit -m "Update Script.ps1" 
+git push -u origin main
+
+#using a lot
+git log --oneline --graph --decorate --all
