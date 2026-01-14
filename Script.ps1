104c505f47532b44d7a8efdf848cb702b0f7cb80
Wuhao Chen <wuhao.chen95@gmail.com>
2026-01-14 10:43:47 +0800

    Update Script.ps1 and create new text file

--- Script.ps1
+++ Script.ps1
@@ -15,12 +15,6 @@ git config --global user.email "wuhao.chen95@gmail.com"
 #many other settings
 git config --global init.defaultBranch main
 
-#standard git cmds
-git status 
-git add -A 
-git commit -m "Update Script.ps1" 
-git push -u origin main
-
 #using a lot
 git log --oneline --graph --decorate --all
 
@@ -29,6 +23,22 @@ function gitgraph {
     git log --oneline --graph --decorate --all    
 }
 
- 
 #compare difference
 #git diff <commit1> .. <commit2>
+
+#standard git cmds
+git status 
+git add -A 
+git commit -m "Update Script.ps1" 
+git push -u origin main
+#create a file
+code ./testfile.txt
+#delete a file, local and staging
+git rm testfile.txt
+#remove all staged content, this is actually using --mixed by default
+git reset
+##reset to the last commit, will potentially lost work
+git reset --hard
+
+#restore version instaged from last commit 
+git restore --staged testfile.txt
